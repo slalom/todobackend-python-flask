514b4987bea69a6f73c3374aeec34a253b5efadc
Oliver Choy <oliver.choy@slalom.com>
2019-03-07 19:40:41 -0800

    Check array length before DELETE

--- hello.py
+++ hello.py
@@ -1,24 +1,25 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
-arr = [] 
+
 
 app = Flask(__name__)
 CORS(app)
-
+arr = []  
 @app.route("/", methods=["GET", "POST", "DELETE"])
 def hello():
+    global arr
     if request.method == "GET": 
-      print(jsonify(arr))
-      return jsonify(arr)
+        
+        return jsonify(arr)
     if request.method == "POST":
-      body = request.get_json()
-      arr.append(body['title'])
-      print(arr)
-      return jsonify(body)
-    if request.method == "DELETE":
-      arr = [] 
-      return arr
+        body = request.get_json()
+        arr.append(body['title'])
 
+        return jsonify(body)
+    if request.method == "DELETE":
+        if len(arr) > 0:
+            arr.pop()   
+        return jsonify(arr)
 
 app.run()
