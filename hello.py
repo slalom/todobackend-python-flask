2062a393987439afba69ac88d16b0c4d41518937
james.meadow <james.meadow@slalom.com>
2019-03-07 19:21:28 -0800

    failed test

--- hello.py
+++ hello.py
@@ -1,16 +1,24 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+
+arr = [] 
+
 app = Flask(__name__)
 CORS(app)
 
 @app.route("/", methods=["GET", "POST", "DELETE"])
 def hello():
-    if request.method == "GET":
-      return "[]"
+    if request.method == "GET": 
+      print(jsonify(arr))
+      return jsonify(arr)
     if request.method == "POST":
       body = request.get_json()
+      arr.append(body['title'])
+      print(arr)
       return jsonify(body)
     if request.method == "DELETE":
-      return ""
+      arr = [] 
+      return arr
+
 
 app.run()
