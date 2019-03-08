79a829f6a6c1fbe8398cfe246097a6d9579430fc
james.meadow <james.meadow@slalom.com>
2019-03-07 18:56:37 -0800

    add delete

--- hello.py
+++ hello.py
@@ -3,12 +3,14 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=["GET", "POST", "DELETE"])
 def hello():
     if request.method == "GET":
       return "Hello World!"
     if request.method == "POST":
       body = request.get_json()
       return jsonify(body)
+    if request.method == "DELETE":
+      return ""
 
 app.run()
