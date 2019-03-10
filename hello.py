ef10911314a9be975799e049e520060127f7b723
james.meadow <james.meadow@slalom.com>
2019-03-10 10:55:06 -0700

    added print to see inputs

--- hello.py
+++ hello.py
@@ -9,17 +9,20 @@ arr = []
 @app.route("/", methods=["GET", "POST", "DELETE"])
 def hello():
     global arr
+
     if request.method == "GET": 
-        
+        print(arr)
         return jsonify(arr)
     if request.method == "POST":
         body = request.get_json()
+        print(body)
         arr.append(body['title'])
-
+        print(arr)	
         return jsonify(body)
     if request.method == "DELETE":
         if len(arr) > 0:
             arr.pop()   
+        print(arr)
         return jsonify(arr)
 
 app.run()
